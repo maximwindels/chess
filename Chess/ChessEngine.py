1d05733255550e0b86371e994e48ce8ebf94fa5c
maximwindels <maxim.windels1@gmail.com>
2025-10-05 12:59:28 +0200

    The board is made with the pieces on it

--- Chess/ChessEngine.py
+++ Chess/ChessEngine.py
@@ -2,7 +2,6 @@
 this file will be responible for handling user input and displaying the current game state
 """
 
-
 class GameState():
     def __init__(self):
         self.board = [
@@ -10,14 +9,14 @@ class GameState():
             #The first character represents the color of the piece, 'b' or 'w'.
             #The second character represents the type of the piece, 'K', 'Q', 'R', 'B', 'N', or 'p'.
             # "--" represents an empty space with no piece.
-            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
-            ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
-            ["--", "--", "--", "--", "--", "--", "--", "--"],
-            ["--", "--", "--", "--", "--", "--", "--", "--"],
-            ["--", "--", "--", "--", "--", "--", "--", "--"],
-            ["--", "--", "--", "--", "--", "--", "--", "--"],
-            ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
-            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
+            ['bR', 'bN', 'bB', 'bQ', 'bK', 'bB', 'bN', 'bR'],
+            ['bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp','bp'],
+            ['--', '--', '--', '--', '--', '--', '--', '--'],
+            ['--', '--', '--', '--', '--', '--', '--', '--'],
+            ['--', '--', '--', '--', '--', '--', '--', '--'],
+            ['--', '--', '--', '--', '--', '--', '--', '--'],
+            ['wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp'],
+            ['wR', 'wN', 'wB', 'wQ', 'wK', 'wB', 'wN', 'wR']
         ]
         self.whitemove = True
         self.moveLog = []
--- Chess/ChessMain.py
+++ Chess/ChessMain.py
@@ -2,3 +2,78 @@
 this class is responsible for storing all the information about a chess game,
 will determine the valid moves at the current state and a move log.
 """
+import pygame
+import pygame as p
+from Chess import ChessEngine
+
+WIDTH = HEIGHT = 512
+DIMENSION = 8
+SQ_SIZE = HEIGHT // DIMENSION
+MAX_FPS = 15 # for animations
+IMAGES = {}
+
+'''
+Initialize a global dictionary of images that will only be called once in the main.
+'''
+
+def loadImages():
+    pieces = ['wp', 'wR', 'wN', 'wB', 'wK', 'wQ','bp', 'bR', 'bK', 'bB', 'bN', 'bQ']
+    for piece in pieces:
+        IMAGES[piece] = p.transform.scale(p.image.load("images/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
+
+'''
+The main driver that will handle user input and output graphics
+'''
+
+def main():
+    p.init()
+    screen = p.display.set_mode((WIDTH, HEIGHT))
+    clock = p.time.Clock()
+    screen.fill(p.Color("white"))
+    gs = ChessEngine.GameState()
+    print(gs.board)
+    loadImages() #only do this once before the while
+    running = True
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+        drawGameState(screen, gs)
+        clock.tick(MAX_FPS)
+        p.display.flip()
+
+'''
+Responsible for all the graphics
+'''
+def drawGameState(screen, gs):
+    drawBoard(screen) # draws the squares on the screen
+    drawPieces(screen, gs.board) # draws ieces ontop of squares
+
+'''
+Draws the squares on the board
+'''
+def drawBoard(screen):
+    colors = [p.Color("light gray"), p.Color("violet")]
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            color = colors[((r+c) % 2)]
+            p.draw.rect(screen, color, p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+'''
+Draws the pieces on the board
+'''
+def drawPieces(screen, board):
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            piece = board[r][c]
+            if piece != '--': # not empty
+                screen.blit(IMAGES[piece], p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
