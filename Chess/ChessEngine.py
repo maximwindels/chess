41234c68cc77d2365d8f5737901b970ea102e96d
maximwindels <maxim.windels1@gmail.com>
2025-10-08 19:47:17 +0200

    pawn can move only elegally for the white ones

--- Chess/ChessEngine.py
+++ Chess/ChessEngine.py
@@ -18,6 +18,9 @@ class GameState():
             ['wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp'],
             ['wR', 'wN', 'wB', 'wQ', 'wK', 'wB', 'wN', 'wR']
         ]
+        self.moveFunctions = {'p': self.getPawnMoves, 'R': self.getRookMoves, 'N': self.getKnightMoves,
+                              'B': self.getBishopMoves, 'Q': self.getQueenMoves, 'K': self.getKingMoves}
+
         self.whitemove = True
         self.moveLog = []
     '''
@@ -55,19 +58,38 @@ class GameState():
                 turn = self.board[r][c][0]
                 if (turn == 'w' and self.whitemove) or (turn == 'b' and not self.whitemove):
                     piece = self.board[r][c][1]
-                    if piece == 'p':
-                        self.getPawnMoves(r, c, moves)
-                    elif piece == 'R':
-                        self.getRookMoves(r, c, moves)
+                    self.moveFunctions[piece](r, c, moves) # calls the appropriate move function based on piece type
         return moves
 
     '''
     Get all the  moves located at row, col and add these moves to the list
     '''
     def getPawnMoves(self, r, c, moves):
-        pass
+        if self.whitemove:
+            if self.board[r-1][c] == "--": #1 square pawn advance
+                moves.append(Move((r, c), (r-1, c), self.board))
+                if r == 6 and self.board[r-2][c] == "--": #2 square pawn advance
+                    moves.append(Move((r, c), (r-2, c), self.board))
+            if c-1 >= 0: # captures to the left
+                if self.board[r-1][c-1][0] == 'b': # enemy piece to capture
+                    moves.append(Move((r, c), (r-1, c-1), self.board))
+            if c+1 <= 7: # captures to the right
+                if self.board[r-1][c+1][0] == 'b': # enemy piece to capture
+                    moves.append(Move((r, c), (r-1, c+1), self.board))
 
     def getRookMoves(self, r, c, moves):
+        pass
+
+    def getKnightMoves(self, r, c, moves):
+        pass
+
+    def getBishopMoves(self, r, c, moves):
+        pass
+
+    def getQueenMoves(self, r, c, moves):
+        pass
+
+    def getKingMoves(self, r, c, moves):
         pass
 
 
