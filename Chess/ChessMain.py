6f046089412dc21b52a4913afd96ffa9e877b1b3
maximwindels <maxim.windels1@gmail.com>
2025-10-11 19:28:32 +0200

    small bug fix

--- Chess/ChessMain.py
+++ Chess/ChessMain.py
@@ -57,8 +57,10 @@ def main():
                     if move in validMoves:
                         gs.makeMove(move)
                         moveMade = True
-                    sqSelected = ()
-                    playerClicks = []
+                        sqSelected = ()
+                        playerClicks = []
+                    else:
+                        playerClicks = [sqSelected]
             elif e.type == p.KEYDOWN:
                 if e.key == p.K_z: #undo when 'z' is pressed
                     gs.undoMove()
